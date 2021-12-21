6a7108328894a1f006a1954680e80fd30ca473ca
tiago369 <tiagobarreto581@gmail.com>
2021-12-21 10:24:35 -0300

    a

--- workbooks_cpp/permutacao.cpp
+++ workbooks_cpp/permutacao.cpp
@@ -1,90 +1,18 @@
 #include<iostream>
+#include<string>
+#include<algorithm>
+
 using namespace std;
 
-void permute_rec(int n ,string txt){
-    int i, a;
-    char aux;
-
-
-    if(n <= 1){
-        cout << txt << endl;
-    }
-    else{   
-        //cout << txt << endl;
-        
-        for(i = 0; i < n; i++){
-            permute_rec(n-1, txt);
-            if(n % 2 == 0){
-                //cout << txt << endl;
-                aux = txt[i];
-                txt[i] = txt[n-1];
-                txt[n-1] = aux;
-            }
-            else{
-                //cout << txt << endl;
-                aux = txt[0];
-                txt[0] = txt[n-1];
-                txt[n-1] = aux;
-            }
-            permute_rec(n-1, txt);
-            cout << txt << endl;
-        }   
-    }
-}
-
-void permute_non_rec(int n, string txt){
-    int c[n], i;
-    char aux;
-
-    for(i = 0; i < n; i++){
-        c[i] = 0;
-    }
-    
-    cout << txt << endl;
-
-    i = 0;
-    while(i < n){
-        if(c[i] < i){
-            if(n % 2 == 0){
-                //cout << txt << endl;
-                aux = txt[i];
-                txt[i] = txt[n-1];
-                txt[n-1] = aux;
-            }
-            else{
-                //cout << txt << endl;
-                aux = txt[0];
-                txt[0] = txt[n-1];
-                txt[n-1] = aux;
-            }
-            cout << txt << endl;
-            c[i]++;
-            i = 0;
-        }
-        else{
-            c[i]=0;
-            i++;
-        }
-    }
-}
-
-void permute(string txt){
-    int n;
-    n = txt.length();
-    permute_non_rec(n, txt);
-}
-
 int main(){
-    
     string txt;
-    getline(cin, txt);
-    permute(txt);
+    cout << "Digite a palavra a ser permutada: ";
+    cin >> txt;
 
-    /*
-    int y;
-    cin >> y;
-    cout << fatorial(y);*/
-
+    sort(txt.begin(), txt.end());
+    do{
+        cout << txt << endl;
+    }while(next_permutation(txt.begin(), txt.end()));
 
     return 0;
 }
