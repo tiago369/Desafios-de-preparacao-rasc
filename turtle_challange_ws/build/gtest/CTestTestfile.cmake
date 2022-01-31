c3f0ccde450a96773ee64ca114d2123c85bbab30
tiago369 <tiagobarreto581@gmail.com>
2022-01-31 12:44:30 -0300

    a

---
+++ turtle_challange_ws/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest
+# Build directory: /home/tiago/Documents/projects/Desafios-de-preparacao-rasc/turtle_challange_ws/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
